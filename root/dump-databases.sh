86aacbfecd55c5f19c0191716cbdecc5b42c54df
Pierre-Yves Rollo <dev@pyrollo.com>
2018-10-21 14:33:23 +0000

    Db password in env and db dump config file

--- root/dump-databases.sh
+++ root/dump-databases.sh
@@ -1,24 +1,67 @@
 #!/bin/sh
 
 wwwroot=/data/www
-dbuser=root
-dbpwd=<dbpassword>
+conffile=$wwwroot/conf/dbdump.conf
+
+if [[ -z "$dbuser" ]] 
+then
+	echo "dbuser env var not set."
+	echo "Aborting."
+	exit 1
+fi
+
+if [[ -z "$dbpwd" ]]
+then
+	echo "dbpwd env var not set."
+	echo "Aborting."
+	exit 1
+fi
+
+if [ ! -r "$conffile" ]
+then
+	echo "Configuration file $conffile not readable."
+	echo "Aborting."
+	exit 1
+fi
 
 log() {
   echo `date +"%Y-%m-%d %H:%M:%S"` $@
 }
 
 dump_db() {
-  log Start dumping database $1.
-  mysqldump -u $dbuser -p$dbpwd $1 -r $wwwroot/$2/$1.sql
-  gzip -f $wwwroot/$2/$1.sql
-  log Finished dumping database $1.
+	log Start dumping database $1.
+	mysqldump -u $dbuser -p$dbpwd $1 -r $2/$1.sql
+	gzip -f $2/$1.sql
+	log Finished dumping database $1.
 }
 
 log Start dumping databases.
 
-# Add here one line per database to export
-dump_db <dbname> <dirname>
-dump_db <dbname> <dirname>
+while read line
+do
+	if [[ "$line" =~ ^(.*)# ]]
+	then
+		line=${BASH_REMATCH[1]}
+	fi
+
+	if [[ "$line" =~ ^[[:space]]*(.*)[[:space:]]*$ ]]
+	then
+		line=${BASH_REMATCH[1]}
+	fi
+	
+	if [[ -z "$line" ]]
+	then
+		continue
+	fi
+
+	if [[ "$line" =~ ^(.+)[[:space:]]+(.+)$ ]]
+	then
+		dump_db ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
+	else
+		echo "Error in $conffile in following line:"
+		echo "$line"
+	fi
+
+done < "$conffile"
 
 log Finished databases dump.
